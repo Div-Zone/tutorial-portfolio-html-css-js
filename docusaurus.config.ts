d774d3fe09f34f3614cd5f58382c6149f5bd8096
Gabriel Maza <gabrielmaza88@gmail.com>
2025-05-23 16:59:40 -0300

    meta tags, md to mdx, gtag plugin removed

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -3,27 +3,15 @@ import type { Config } from "@docusaurus/types";
 import type * as Preset from "@docusaurus/preset-classic";
 
 const config: Config = {
-  title: "DivZone: Desarrollo De Software y Cursos Online",
+  title: "DivZone",
   tagline: "Documentación",
   favicon: "img/favicon.ico",
-
-  // Set the production url of your site here
   url: "https://docs.div.zone",
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/",
-
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: "DivZone", // Usually your GitHub org/user name.
-  projectName: "divzone", // Usually your repo name.
-
+  organizationName: "DivZone",
+  projectName: "divzone",
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
-
-  // Even if you don't use internationalization, you can use this field to set
-  // useful metadata like html lang. For example, if your site is Chinese, you
-  // may want to replace "en" with "zh-Hans".
   i18n: {
     defaultLocale: "es",
     locales: ["es"],
@@ -48,8 +36,15 @@ const config: Config = {
   ],
 
   themeConfig: {
-    // Replace with your project's social card
     image: "img/divzone-social-card.jpg",
+    metadata: [
+      { name: "author", content: "Gabriel Maza" },
+      {
+        name: "keywords",
+        content: "HTML, CSS, javascript, tutoriales, desarrollo web",
+      },
+      { name: "publisher", content: "div.zone" },
+    ],
     navbar: {
       title: "DivZone Docs",
       logo: {
